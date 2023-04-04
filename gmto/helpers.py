25e6f354fbd93475927900a92c80e2c4969c5015
DiegoD <doimo.diego@gmail.com>
2023-04-04 16:19:54 +0200

    u

--- gmto/helpers.py
+++ gmto/helpers.py
@@ -3,12 +3,37 @@ import os.path
 
 
 def read_empire_data(path: str):
+    """Reads the Empire .json file.
+
+    Parameters
+    ----------
+    path : str
+        path to the EMpire '.json' file.
+
+    Returns
+    -------
+    dict
+        Empire data.
+    """
+
     with open(path, "r") as file:
         empire_data = json.load(file)
     return empire_data
 
 
 def read_falcon_data(path: str):
+    """Reads the Millenium Falcon .json file. Makes the path to the routes database absolute.
+
+    Parameters
+    ----------
+    path : str
+        path to the Millenium Falcon '.json' file.
+
+    Returns
+    -------
+    dict
+        Millenium Falcon data.
+    """
     with open(path, "r") as file:
         falcon_data = json.load(file)
 
